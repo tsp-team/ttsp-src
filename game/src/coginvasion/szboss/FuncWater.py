e09ca4334ae2badacb2846e51a6e2070d681b399
Nathan S <nathanstgeorge7@gmail.com>
2020-05-17 23:24:44 -0400

    Update FuncWater.py

--- game/src/coginvasion/szboss/FuncWater.py
+++ game/src/coginvasion/szboss/FuncWater.py
@@ -1,6 +1,6 @@
 from panda3d.core import NodePath, Point3
 
-from Entity import Entity
+from .Entity import Entity
 
 class FuncWater(Entity):
 
@@ -10,16 +10,16 @@ class FuncWater(Entity):
 
     def load(self):
         Entity.load(self)
-        
+
         specname = self.getEntityValue("waterspec")
         spec = base.waterReflectionMgr.getDefaultSpec(specname)
 
         mins = Point3(0)
         maxs = Point3(0)
         self.cEntity.fillinBounds(mins, maxs)
-        
+
         pos = Point3((mins[0] + maxs[0]) / 2, (mins[1] + maxs[1]) / 2, maxs[2])
-        
+
         self.waterNode = base.waterReflectionMgr.addWaterNode(
             (mins.getX() - pos[0], maxs.getX() - pos[0], mins.getY() - pos[1], maxs.getY() - pos[1]),
             pos,
