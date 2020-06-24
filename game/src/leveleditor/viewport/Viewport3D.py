leveleditor: fix fly cam rotation, fix incorrect 3D gizmo orientation

game/src/leveleditor/viewport/Viewport3D.py
@@ -15,8 +15,10 @@ class Viewport3D(Viewport):
     def tick(self):
         Viewport.tick(self)
         if self.gizmo:
-            hpr = self.cam.getHpr(render)
-            self.gizmo.np.setHpr(hpr.x, hpr.y, 0)
+            quat = render.getQuat(self.cam)
+            self.gizmo.xNp.lookAt(quat.getRight())
+            self.gizmo.yNp.lookAt(quat.getForward())
+            self.gizmo.zNp.lookAt(quat.getUp())
 
     def initialize(self):
         Viewport.initialize(self)

game/src/leveleditor/viewport/ViewportGizmo.py
@@ -1,4 +1,4 @@
-from panda3d.core import LineSegs, Vec4, Point3, TextNode
+from panda3d.core import LineSegs, Vec4, Point3, TextNode, Vec3
 
 # Widget that shows which direction the camera is looking in the 3D viewport.
 class ViewportGizmo:
@@ -6,49 +6,50 @@ class ViewportGizmo:
     def __init__(self, vp):
         self.vp = vp
         axes = self.vp.getGizmoAxes()
-        segs = LineSegs()
 
         self.np = self.vp.a2dBottomLeft.attachNewNode("viewAxisWidget")
-        self.np.setScale(0.15)
-        self.np.setPos(0.16, 0, 0.16)
+        self.np.setScale(0.14)
+        self.np.setPos(0.19, 0, 0.19)
 
         if 0 in axes:
-          # X line
-          segs.setColor(Vec4(1, 0, 0, 1))
-          segs.moveTo(Point3(0))
-          segs.drawTo(Point3(1, 0, 0))
-          xText = TextNode('yText')
-          xText.setTextColor(Vec4(1, 0, 0, 1))
-          xText.setText('X')
-          xTextNp = self.np.attachNewNode(xText.generate())
-          xTextNp.setX(1.05)
-          xTextNp.setBillboardPointEye()
-          xTextNp.setScale(0.5)
+            # X line
+            self.xNp = self.makeGizmoAxis(0, "X", 1.2)
 
         if 1 in axes:
-          # Y line
-          segs.setColor(Vec4(0, 1, 0, 1))
-          segs.moveTo(Point3(0))
-          segs.drawTo(Point3(0, -1, 0))
-          yText = TextNode('yText')
-          yText.setTextColor(Vec4(0, 1, 0, 1))
-          yText.setText('Y')
-          yTextNp = self.np.attachNewNode(yText.generate())
-          yTextNp.setY(-1.05)
-          yTextNp.setBillboardPointEye()
-          yTextNp.setScale(0.5)
+            # Y line
+            self.yNp = self.makeGizmoAxis(1, "Y")
 
         if 2 in axes:
-          # Z line
-          segs.setColor(Vec4(0, 0, 1, 1))
-          segs.moveTo(Point3(0))
-          segs.drawTo(Point3(0, 0, 1))
-          zText = TextNode('zText')
-          zText.setTextColor(Vec4(0, 0, 1, 1))
-          zText.setText('Z')
-          zTextNp = self.np.attachNewNode(zText.generate())
-          zTextNp.setZ(1.05)
-          zTextNp.setBillboardPointEye()
-          zTextNp.setScale(0.5)
+            # Z line
+            self.zNp = self.makeGizmoAxis(2, "Z")
 
-        self.np.attachNewNode(segs.create())
+    def makeGizmoAxis(self, axis, text, textOffset = 1.1):
+        color = Vec4(0, 0, 0, 1)
+        color[axis] = 1
+
+        pos = Vec3(0, 1, 0)
+        if axis == 1:
+            pos[1] = -pos[1]
+
+        if axis == 1:
+            textOffset = -textOffset
+
+        direction = Vec3(0)
+        direction[axis] = 1
+
+        segs = LineSegs()
+        segs.setColor(color)
+        segs.moveTo(Point3(0))
+        segs.drawTo(pos)
+        np = self.np.attachNewNode(segs.create())
+        np.lookAt(direction)
+        tn = TextNode('gizmoAxis%iText' % axis)
+        tn.setTextColor(color)
+        tn.setAlign(TextNode.ACenter)
+        tn.setText(text)
+        tnnp = np.attachNewNode(tn.generate())
+        tnnp.setY(textOffset)
+        tnnp.setBillboardPointEye()
+        tnnp.setScale(0.5)
+
+        return np