66db0952818ab01abf1d3a9512046ef9bd4d94d5
Brian Lach <brianlach72@gmail.com>
2020-07-02 19:38:43 -0400

    leveleditor: more work on object properties panel

--- game/src/leveleditor/SelectionManager.py
+++ game/src/leveleditor/SelectionManager.py
@@ -18,24 +18,46 @@ Bounds3DState = RenderState.make(
 Bounds2DState = RenderModes.DashedLineNoZ()
 Bounds2DState = Bounds2DState.setAttrib(ColorAttrib.makeFlat(Vec4(1, 1, 0, 1)))
 
+MultipleValues = "<multiple values>"
+MultipleEntities = "<multiple entities>"
+
 class ObjectPropertiesItem(QtGui.QStandardItem):
 
-    def __init__(self, win):
+    def __init__(self, win, entities, propName, isKey):
         QtGui.QStandardItem.__init__(self)
-        #self.valueItem = valueItem
-        #self.entity = entity
+        self.win = win
+        self.entities = entities
+        self.pairing = None
+        self.propName = propName
+        self.prop = entities[0].metaData.property_by_name(propName)
+        self.isKey = isKey
+
+        if self.isKey:
+            self.setEditable(False)
+            if self.prop.display_name and len(self.prop.display_name) > 0:
+                self.setText(self.prop.display_name)
+            else:
+                self.setText(self.propName)
+        else:
+            self.setEditable(True)
+            self.computeValueText()
+
+    def computeValueText(self):
+        value = None
+        for ent in self.entities:
+            entVal = str(ent.entityData[self.propName])
+            if value is not None and entVal != value:
+                # There are different values across entities for this property.
+                value = MultipleValues
+                break
+            elif value is None:
+                value = entVal
+        self.setText(value)
 
     def data(self, role):
         if role == QtCore.Qt.TextAlignmentRole:
             return QtCore.Qt.AlignLeft
-        #elif role == QtCore.Qt.DisplayRole:
-            #return
         return QtGui.QStandardItem.data(self, role)
-
-class ObjectPropertiesValue(QtGui.QStandardItem):
-
-    def __init__(self, win):
-        QtGui.QStandardItem.__init__(self, win)
 
 class ObjectPropertiesModel(QtGui.QStandardItemModel):
 
@@ -58,35 +80,107 @@ class ObjectPropertiesWindow(QtWidgets.QDockWidget):
         self.setWidget(w)
 
         self.propertiesModel = ObjectPropertiesModel(self)
+        self.ui.propertiesView.setMouseTracking(True)
         self.ui.propertiesView.setModel(self.propertiesModel)
         self.ui.propertiesView.header().setDefaultAlignment(QtCore.Qt.AlignCenter)
+        self.ui.propertiesView.clicked.connect(self.__propertyClicked)
+        self.ui.lePropertyFilter.textChanged.connect(self.__filterProperties)
+        self.ui.comboClass.currentIndexChanged.connect(self.__changeEntityClass)
 
         self.updateAvailableClasses()
 
         base.qtWindow.addDockWindow(self)
         self.hide()
 
+    def __changeEntityClass(self, idx):
+        classname = self.ui.comboClass.currentText()
+        for ent in self.mgr.selectedObjects:
+            ent.setClassname(classname)
+        self.updateForSelection()
+        self.mgr.updateSelectionBounds()
+
+    def __propertyClicked(self, idx):
+        item = self.propertiesModel.itemFromIndex(idx)
+        self.updatePropertyDetailText(item.prop)
+
+    def updatePropertyDetailText(self, prop):
+        if prop.display_name and len(prop.display_name) > 0:
+            self.ui.lblPropertyName.setText(prop.display_name)
+        else:
+            self.ui.lblPropertyName.setText(prop.name)
+
+        if prop.description and len(prop.description) > 0:
+            self.ui.lblPropertyDesc.setText(prop.description)
+        else:
+            self.ui.lblPropertyDesc.setText("")
+
+    def __filterProperties(self, text):
+        if len(text) == 0:
+            # Empty filter, show everything
+            for i in range(self.propertiesModel.rowCount()):
+                self.ui.propertiesView.setRowHidden(i, QtCore.QModelIndex(), False)
+            return
+
+        # First hide all rows...
+        for i in range(self.propertiesModel.rowCount()):
+            self.ui.propertiesView.setRowHidden(i, QtCore.QModelIndex(), True)
+
+        # ...then show all rows containing the filter string
+        items = self.propertiesModel.findItems(text, QtCore.Qt.MatchContains)
+        for item in items:
+            self.ui.propertiesView.setRowHidden(item.row(), QtCore.QModelIndex(), False)
+
     def updateForSelection(self):
         numSelections = self.mgr.getNumSelectedObjects()
 
         if numSelections == 0:
             self.hide()
+            return
         else:
             self.show()
 
-        if numSelections == 1:
-            selection = base.selectionMgr.selectedObjects[0]
-            self.ui.comboClass.setCurrentText(selection.classname)
-            self.propertiesModel.removeRows(0, self.propertiesModel.rowCount())
-            rowIdx = 0
-            for prop, value in selection.entityData.items():
-                propItem = ObjectPropertiesItem(self)
-                propItem.setText(prop)
-                valueItem = ObjectPropertiesItem(self)
-                valueItem.setText(str(value))
-                self.propertiesModel.setItem(rowIdx, 0, propItem)
-                self.propertiesModel.setItem(rowIdx, 1, valueItem)
-                rowIdx += 1
+        # Clear our filtering
+        self.ui.lePropertyFilter.clear()
+
+        hasMultipleClassnames = False
+        classname = None
+        desc = ""
+        propName2entities = {}
+
+        # Determine the classname to display based on our selections
+        for i in range(numSelections):
+            selection = self.mgr.selectedObjects[i]
+            entClassname = selection.metaData.name
+            if classname is not None and entClassname != classname and not hasMultipleClassnames:
+                # Multiple entity classnames
+                classname = MultipleEntities
+                desc = ""
+                hasMultipleClassnames = True
+            elif not hasMultipleClassnames:
+                classname = entClassname
+                if selection.metaData.description:
+                    desc = selection.metaData.description
+
+            for prop in selection.entityData.keys():
+                if not prop in propName2entities:
+                    propName2entities[prop] = [selection]
+                else:
+                    propName2entities[prop].append(selection)
+
+        self.ui.lblPropertyName.setText(classname)
+        self.ui.lblPropertyDesc.setText(desc)
+        self.ui.comboClass.setCurrentText(classname)
+
+        self.propertiesModel.removeRows(0, self.propertiesModel.rowCount())
+        rowIdx = 0
+        for prop, entityList in propName2entities.items():
+            propItem = ObjectPropertiesItem(self, entityList, prop, True)
+            valueItem = ObjectPropertiesItem(self, entityList, prop, False)
+            valueItem.pairing = propItem
+            propItem.pairing = valueItem
+            self.propertiesModel.setItem(rowIdx, 0, propItem)
+            self.propertiesModel.setItem(rowIdx, 1, valueItem)
+            rowIdx += 1
 
     def updateAvailableClasses(self):
         self.ui.comboClass.clear()
